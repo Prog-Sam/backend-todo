6742305f58026ff9bed616846c7a3e9ee3814090
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2024-04-18 18:17:26 +0800

    updated the port from 3000 to 3001

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 //insert startup codes
 require('./startup/routes')(app);
 
-//checking if port environment variable is set, if not, use 3000
-const port = process.env.PORT || 3000;
+//checking if port environment variable is set, if not, use 3001
+const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => {
     console.log(`Listening on Port: ${port}`);
